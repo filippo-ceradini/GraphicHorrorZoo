19b95a87ff49a4473234f00ff5bcb69a6d6e8ae7
filippo <filippo-ceradini>
2021-09-13 10:37:43 +0200

    change adding if statements

--- src/main.java
+++ src/main.java
@@ -3,19 +3,25 @@ public class main {
     public static void main(String[] args) {
     snake snake1 = new snake(9,9);
     rabbit rabbit1 = new rabbit(2,2);
-
+    if( rabbit1.xrabb == --snake1.ysnak && rabbit1.yrabb == --snake1.ysnak){
+        System.out.println(rabbit1.getRabbyell());
+    }
     System.out.println(rabbit1.toRabbpos());
     System.out.println(snake1.toSnakepos());
-    for(int i=1; i<4;i++) {
+    for(int i=1; i<6;i++) {
         snake1.setXsnak(--snake1.xsnak);
         snake1.setYsnak(--snake1.ysnak);
         System.out.println(snake1.toSnakepos());
         rabbit1.setXrabb(++rabbit1.xrabb);
         rabbit1.setYrabb(++rabbit1.yrabb);
         System.out.println(rabbit1.toRabbpos());
+        if( rabbit1.xrabb == snake1.ysnak && rabbit1.yrabb == --snake1.ysnak){
+            System.out.println(rabbit1.getRabbyell());
+        }
+        if( rabbit1.xrabb == snake1.ysnak && rabbit1.yrabb == snake1.ysnak){
+            System.out.println(snake1.getSnakyell());
+        }
     }
-
-        System.out.println(rabbit1.getRabbyell());
         snake1.setXsnak(--snake1.xsnak);
         snake1.setYsnak(--snake1.ysnak);
         System.out.println(snake1.toSnakepos());
