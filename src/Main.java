50553d5369dfbf93efc016b7c4748f866c58331e
filippo <filippo-ceradini>
2021-09-21 11:16:00 +0200

    used class animal to simplify rabbit and snake

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,90 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        /*int rand1 = (int)Math.floor(Math.random()*(8));
-        int rand2 = (int)Math.floor(Math.random()*(8));
-        int rand3 = (int)Math.floor(Math.random()*(8));
-        int rand4 = (int)Math.floor(Math.random()*(8));*/
 
-        animal snake = new animal(9, 9, "Snake says: \"Yo dead bunny!\"", "Snake");
-        animal bunny = new animal(0, 0, "Rabbit says: \"Oh not again!\"", "Rabbit");
-
+        //Graphic for the animals: 3 lines defining them.
+        AnimGraph gSnake = new AnimGraph("|   Y   ", "|  (\")  ", "|   \\\\  ");
+        AnimGraph gRabbit = new AnimGraph("| (\\_/) ", "| (o.o) ", "| (___)0");
+        //Animals: position x&y, what the animal yells and the name of the animal
+        animal snake = new animal(9, 9, "+—————————————————————————————+\n" +
+                "|   “You’re Dead Bunny!       |\n" +
+                "|      ---_ ......._-_--.     |    \n" +
+                "|     (|\\ /      / /| \\  \\    |\n" +
+                "|     /  /     .'  -=-'   `.  |\n" +
+                "|    /  /    .'             ) |\n" +
+                "|  _/  /   .'        _.)   /  |\n" +
+                "| / o   o        _.-' /  .'   |\n" +
+                "| \\          _.-'    / .'*|   |\n" +
+                "|  \\______.-'//    .'.' \\*|   |\n" +
+                "|   \\|  \\ | //   .'.' _ |*|   |\n" +
+                "|    `   \\|//  .'.'_ _ _|*|   |\n" +
+                "|     .  .// .'.' | _ _ \\*|   |\n" +
+                "|     \\`-|\\_/ /    \\ _ _ \\*\\  |\n" +
+                "|      `/'\\__/      \\ _ _ \\*\\ |\n" +
+                "|     /^|            \\ _ _ \\*\\|\n" +
+                "|    '  `             \\ _ _ \\*| \n" +
+                "|                      \\ - - \\|\n" +
+                "+————————————————————————————-+", "Snake", gSnake);
+        animal bunny = new animal(1, 1, "+—————————————————————————————+\n" +
+                "|                         .\". |\n" +
+                "|   “Oh, not again!”     /  | |\n" +
+                "|              \\        /  /  |\n" +
+                "|               \\      / ,\"   |\n" +
+                "|          .-------.--- /     |\n" +
+                "|         \"._ __.-/ o. o\\     | \n" +
+                "|            \"   (    Y  )    |\n" +
+                "|                 )     /     |\n" +
+                "|                /     (      |\n" +
+                "|               /       Y     |\n" +
+                "|           .-\"         |     |\n" +
+                "|          /  _     \\    \\    |\n" +
+                "|         /    `. \". ) /' )   |\n" +
+                "|        Y       )( / /(,/    |\n" +
+                "|       ,|      /     )       |\n" +
+                "|      ( |     /     /        |\n" +
+                "|       \" \\_  (__   (__       |\n" +
+                "|           “-._,)—._,_)      |\n" +
+                "+————————————————————————————-+", "Rabbit", gRabbit);
+        //The system prints out the position of the animal
         System.out.println(snake.toAnimalPos());
         System.out.println(bunny.toAnimalPos());
+        Graphic.gridAnim(bunny, snake);
 
-            while (bunny.getXpos() != snake.getXpos() && bunny.getYpos() != snake.getYpos()) {
-                bunny.moveFwrd();
-                if (bunny.getXpos() == snake.getXpos() - 1 && bunny.getYpos() == snake.getYpos() - 1) {
-                    System.out.println(bunny.getAnimalYell());
-                    snake.moveBack();
-                    break;
-                }
-                if (bunny.getXpos() == snake.getXpos() && bunny.getYpos() == snake.getYpos()) {break;}
+        Scanner userInput = new Scanner(System.in);
+        //While cycle: until the snake and the rabbit have the same coordinates,
+        // the rabbit will move forward and the snake will move backwards.
+        //2 break methods have been added so no matter if either animal is in an odd or even space,
+        // the snake is the last to move
+        while (bunny.getXpos() != snake.getXpos() && bunny.getYpos() != snake.getYpos()) {
+            bunny.moveFwrd();
+            Graphic.gridAnim(snake, bunny);
+            if (bunny.getXpos() == snake.getXpos() - 1 && bunny.getYpos() == snake.getYpos() - 1) {
+                System.out.println(bunny.getAnimalYell());
+                String input = userInput.nextLine();
+                if (!input.isEmpty()) {}
+
                 snake.moveBack();
-                if (bunny.getXpos() == snake.getXpos() - 1 && bunny.getYpos() == snake.getYpos() - 1) {
-                    System.out.println(bunny.getAnimalYell());}
-
+                Graphic.gridAnim(snake, bunny);
+                break;
             }
-            System.out.println(snake.getAnimalYell());
-        //if (bunny.getPos() ==snake.getPos()){System.out.println("equal");}
+            if (bunny.getXpos() == snake.getXpos() && bunny.getYpos() == snake.getYpos()) {break;}
+            snake.moveBack();
+            Graphic.gridAnim(snake, bunny);
+            if (bunny.getXpos() == snake.getXpos() - 1 && bunny.getYpos() == snake.getYpos() - 1) {
+                System.out.println(bunny.getAnimalYell());
+                String input = userInput.nextLine();
+                if (!input.isEmpty()) {}
+            }
+        }
+
+
+        System.out.println(snake.getAnimalYell());
+
     }
 }
+
+
 
 
 
