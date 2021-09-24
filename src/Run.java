c2229aafc6d8c6c4c75f490cd44df22a686750f5
filippo-ceradini <90599585+filippo-ceradini@users.noreply.github.com>
2021-09-24 08:45:03 +0200

    Update Run.java

--- src/Run.java
+++ src/Run.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 
 public class Run {
-    public Run() {
-    }
-
     public static void main(String[] var0) {
-        String var1 = """
+
+        //Scanner to wait for user input
+        Scanner userInput = new Scanner(System.in);
+
+        //Printsout of what the animals say when the conditions ask for it
+        String yellSnake = """
                 +—————————————————————————————+
                 |   “You’re Dead Bunny!       |
                 |      ---_ ......._-_--.     |   \s
@@ -25,46 +27,71 @@ public class Run {
                 |    '  `             \\ _ _ \\*|\s
                 |                      \\ - - \\|
                 +————————————————————————————-+""";
-        String var2 = "+—————————————————————————————+\n|                         .\". |\n|   “Oh, not again!”     /  | |\n|              \\        /  /  |\n|               \\      / ,\"   |\n|          .-------.--- /     |\n|         \"._ __.-/ o. o\\     | \n|            \"   (    Y  )    |\n|                 )     /     |\n|                /     (      |\n|               /       Y     |\n|           .-\"         |     |\n|          /  _     \\    \\    |\n|         /    `. \". ) /' )   |\n|        Y       )( / /(,/    |\n|       ,|      /     )       |\n|      ( |     /     /        |\n|       \" \\_  (__   (__       |\n|           “-._,)—._,_)      |\n+————————————————————————————-+";
-        AnimGraph var3 = new AnimGraph("|   Y   ", "|  (\")  ", "|   \\\\  ");
-        AnimGraph var4 = new AnimGraph("| (\\_/) ", "| (o.o) ", "| (___)0");
-        animal var5 = new animal(9, 9, var1, "Snake", var3);
-        animal var6 = new animal(1, 1, var2, "Rabbit", var4);
-        System.out.println(var5.toAnimalPos());
-        System.out.println(var6.toAnimalPos());
-        Graphic.gridAnim(var6, var5);
-        Scanner var7 = new Scanner(System.in);
+        String yellBunny = """
+                +—————————————————————————————+
+                |                         .". |
+                |“C'mon not again!”      /  | |
+                |              \\        /  /  |
+                |               \\      / ,"   |
+                |          .-------.--- /     |
+                |         "._ __.-/ o. o\\     |\s
+                |            "   (    Y  )    |
+                |                 )     /     |
+                |                /     (      |
+                |               /       Y     |
+                |           .-"         |     |
+                |          /  _     \\    \\    |
+                |         /    `. ". ) /' )   |
+                |        Y       )( / /(,/    |
+                |       ,|      /     )       |
+                |      ( |     /     /        |
+                |       " \\_  (__   (__       |
+                |           “-._,)—._,_)      |
+                +————————————————————————————-+""";
 
-        while(var6.getXpos() != var5.getXpos() && var6.getYpos() != var5.getYpos()) {
-            var6.moveFwrd();
-            Graphic.gridAnim(var5, var6);
-            String var8;
-            if (var6.getXpos() == var5.getXpos() - 1 && var6.getYpos() == var5.getYpos() - 1) {
-                System.out.println(var6.getAnimalYell());
-                var8 = var7.nextLine();
-                if (!var8.isEmpty()) {
-                }
+        String input;
+
+        //AnimGraph is an object that contains the building blocks to print the cell with the animal
+        //on the grid. It's basically an array.
+        AnimGraph boxSnake = new AnimGraph("|   Y   ", "|  (\")  ", "|   \\\\  ");
+        AnimGraph boxBunny = new AnimGraph("| (\\_/) ", "| (o.o) ", "| (___)0");
 
 
-                var5.moveBack();
-                Graphic.gridAnim(var5, var6);
+        //Animal is the class that represents the animal, both have the position,
+        //yell, name and the object anim graph is associated to them
+        animal snake = new animal(9, 9, yellSnake, "Snake", boxSnake);
+        animal bunny = new animal(1, 1, yellBunny, "Rabbit", boxBunny);
+
+        //The program prints out the position of the animals at the beginning
+        System.out.println(snake.toAnimalPos());
+        System.out.println(bunny.toAnimalPos());
+
+        //after this the first print of the grid is printed
+        //to show the grid one at the time(there is a scanner in the Graphic object
+        Graphic.gridAnim(snake, bunny);
+
+        while(bunny.getXpos() != snake.getXpos() && bunny.getYpos() != snake.getYpos()) {
+            bunny.moveFwrd();
+            Graphic.gridAnim(snake, bunny);
+            if (bunny.getXpos() == snake.getXpos() - 1 && bunny.getYpos() == snake.getYpos() - 1) {
+                System.out.println(bunny.getAnimalYell());
+                String wait4input = userInput.nextLine();
+                snake.moveBack();
+                Graphic.gridAnim(snake, bunny);
                 break;
             }
 
-            if (var6.getXpos() == var5.getXpos() && var6.getYpos() == var5.getYpos()) {
+            if (bunny.getXpos() == snake.getXpos() && bunny.getYpos() == snake.getYpos()) {
                 break;
             }
 
-            var5.moveBack();
-            Graphic.gridAnim(var5, var6);
-            if (var6.getXpos() == var5.getXpos() - 1 && var6.getYpos() == var5.getYpos() - 1) {
-                System.out.println(var6.getAnimalYell());
-                var8 = var7.nextLine();
-                if (!var8.isEmpty()) {
+            snake.moveBack();
+            Graphic.gridAnim(snake, bunny);
+            if (bunny.getXpos() == snake.getXpos() - 1 && bunny.getYpos() == snake.getYpos() - 1) {
+                System.out.println(bunny.getAnimalYell());
+                String wait4input = userInput.nextLine();
                 }
             }
-        }
-
-        System.out.println(var5.getAnimalYell());
+        System.out.println(snake.getAnimalYell());
     }
 }
